6f3d7258d8e0b3c601244fe1f89f82abe53951e7
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2023-09-28 10:56:56 -0300

    Update Ex3.c

--- Senai/C Array 26_09 e 27_09/Ex3.c
+++ Senai/C Array 26_09 e 27_09/Ex3.c
@@ -4,8 +4,41 @@
 #include<stdio.h>
 int main()
 {
-    FILE * arquivo;
-    int op;
-    char array[3];
+   
+    
+    char nome[10], curso[20], media[5];
+    char array[6][20] = {"Nome:  1", "  Curso: ", "3", "  Média: ", "4\n" };
+    char *ptr = array;
+    char *ptr2 = nome;
+
+    printf("Digite o nome do aluno: ");
+    scanf("%s", &nome);
+
+    printf("Digite o curso do aluno: ");
+    scanf("%s", &curso);
+
+    printf("Digite a média do aluno: ");
+    scanf("%s", &media);
+
+
+
+        *(ptr + 7) = nome;
+    
+   
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%c", array[i][j]);
+        }  
+    }
+
+    
+    
+    
+    
+
+
+
 
 }
