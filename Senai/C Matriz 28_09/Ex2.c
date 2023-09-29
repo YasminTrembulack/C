a4bb17151869b3d63cadb1af8f22ca6e0495b3b8
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2023-09-29 10:50:51 -0300

    Create Ex2.c

---
+++ Senai/C Matriz 28_09/Ex2.c
@@ -0,0 +1,19 @@
+#include "functions.h"
+
+
+
+int main()
+{
+    char matrix[3][3] = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
+
+    mostrarMatrix(&matrix);
+}
+switch (expression)
+{
+case /* constant-expression */:
+    /* code */
+    break;
+
+default:
+    break;
+}
