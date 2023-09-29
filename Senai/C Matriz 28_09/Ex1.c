f07901f622ab8f960acee7212f71f9f96e347a86
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2023-09-29 10:50:54 -0300

    Update Ex1.c

--- Senai/C Matriz 28_09/Ex1.c
+++ Senai/C Matriz 28_09/Ex1.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {   
-    char materia[4][3] = {"Mat", "Bio", "Fis", "His"};
-    int matrix[4][3];
+    int matrix[3][3];
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
     {
         printf("Digite a nota de uma materia: \n");
         for (int j = 0; j< 3; j++)
@@ -13,19 +12,17 @@ int main()
         }
     }
 
-    for (int i = 0; i < 4; i++)
+
+    for (int j = 0; j < 4; j++)
     {
-        for (int j = 0; j < 4; j++)
-        {
-            for (int k = 0; k < 3; k++)
-            {
-                /* code */
-            }
-            
+        for (int k = 0; k < 3; k++)
+        {       
+            printf("%c", matrix[j][k]); 
         }
-        
+         printf("\n");   
     }
+        
+}
     
 
 
-}
