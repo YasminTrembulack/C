bd175723ac78efe8adfe34454b7c78a1c66b2e08
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2023-10-02 09:51:44 -0300

    Update Ex2.c

--- Senai/C Matriz 28_09/Ex2.c
+++ Senai/C Matriz 28_09/Ex2.c
@@ -4,9 +4,9 @@
 
 int main()
 {
-    char matrix[3][3] = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
+    char matriz[3][3] = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
 
-    mostrarMatrix(&matrix);
+    mostrarMatrix(&matriz);
 }
 switch (expression)
 {
