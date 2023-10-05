e39156e700570fe71acb654e9f25b6cb9981edb2
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2023-10-05 08:36:46 -0300

    Create Coisas da minha cabeça.c

---
+++ Senai/C Malloc 03_10/Coisas da minha cabeça.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int coluna = 3, linha= 3;
+
+    // printf("Digite a quantidade de linhas: ");
+    // scanf("%i", &linha);
+
+    // printf("Digite a quantidade de colunas: ");
+    // scanf("%i", &coluna);
+
+    int *matriz = malloc(coluna * linha * sizeof(int));
+    
+    
+
+}
