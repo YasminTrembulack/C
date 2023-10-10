cc9048f47db4005ee1e5ef860a8b746bb4096ceb
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2023-10-10 09:21:48 -0300

    Create Ex2.c

---
+++ Senai/C Structs 09_10/Ex2.c
@@ -0,0 +1,37 @@
+// 2.Construa uma estrutura aluno com nome, numero de matrícula e curso.
+// Leia do usuário a informação de 5 alunos, armazene em vetor dessa
+// estrutura e imprima os dados na tela.
+
+#include <stdio.h>
+typedef struct 
+{
+    char Nome[10];
+    int Nmatricula;
+    char Curso[20];
+}
+Aluno;
+
+int main()
+{   
+    Aluno pessoas[5];
+    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Digite o nome do aluno: ");
+        scanf("%s", pessoas[i].Nome);
+
+        printf("Digite o número da matricula: ");
+        scanf("%i", &pessoas[i].Nmatricula);
+
+        printf("Qual o curso? ");
+        scanf("%s", pessoas[i].Curso);
+    }
+    
+    for ( int i = 0; i < 5; i++)
+    {
+       printf("Nome: %s  Numero Matricula: %i  Curso: %s\n", pessoas[i].Nome, pessoas[i].Nmatricula, pessoas[i].Curso);
+    }
+    
+
+
+}
